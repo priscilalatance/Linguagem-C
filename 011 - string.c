cabdb151874108cb951c2603217f289fb8b88782
priscilalatance <88410500+priscilalatance@users.noreply.github.com>
2022-01-09 20:58:58 -0300

    Add files via upload

---
+++ 011 - string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+   char aluno[21], senha[20];
+   printf("\nInforme um nome: ");
+   scanf("%[^\n]s",aluno);
+   //gets(aluno);
+   //teste="Igor"; NÃO DÁ CERTO
+   strcpy(senha,"Igor");
+
+   printf("\nAluno: %s\n",aluno);
+   printf("Teste: %s\n",senha);
+   printf("Tamanho da String Aluno: %d\n",strlen(aluno));
+   for (int i=0;i<strlen(aluno);i++)
+        printf("%c-", aluno[i]);
+
+   //if (senha=="Igor")  não funciona
+   if (strcmp(senha,"Igor")==0)
+       printf("\nOK");
+   else
+       printf("\nSenha errada");
+   printf("\n%s comeca com a letra %c",aluno,aluno[0]);
+
+}
