4c733dd15b4da6b33e288da066ff3bc319bdcb9f
priscilalatance <88410500+priscilalatance@users.noreply.github.com>
2022-01-15 15:14:14 -0300

    Update VetorComodo4Pri.c

--- VetorComodo4Pri.c
+++ VetorComodo4Pri.c
@@ -5,36 +5,36 @@ int main(){
   int TotComodos, Cont=1, Classe;
   float Area, Potencia=0, Lampadas=0, SomaPotencia=0, SomaLampadas=0;
 
-    printf("Informe o Total de Comodos da Casa: ");
-    scanf("%d",&TotComodos);
+  printf("Informe o Total de Comodos da Casa: ");
+  scanf("%d",&TotComodos);
 
-    while (Cont<=TotComodos){
-        printf("Digite a Classe do Comodo: ");
-        scanf("%d",&Classe);
-        printf("Digite a Area do Comodo: ");
-        scanf("%f",&Area);
-            if (Classe==1){
-                Potencia=vetor[0]*Area;
-            }
-            if (Classe==2){
-                Potencia=vetor[1]*Area;
-            }
-            if (Classe==3){
-                Potencia=vetor[2]*Area;
-            }
-            if (Classe==4){
-                Potencia=vetor[3]*Area;
-            }
-            if (Classe==5){
-                Potencia=vetor[4]*Area;
-            }
-            Lampadas=Potencia/60;
-            printf("Potencia do Comodo: %.f\n", Potencia);
-            printf("Lampadas Necessarias para o Comodo: %.f\n", Lampadas);
-            SomaPotencia=SomaPotencia+Potencia;
-            SomaLampadas=SomaLampadas+Lampadas;
-            Cont=Cont+1;
-    }
-    printf("Total de Potencia da Residencia: %.f\n", SomaPotencia);
-    printf("Total de Lampadas Necessarias para a Residencia: %.f\n", SomaLampadas);
+  while (Cont<=TotComodos){
+      printf("Digite a Classe do Comodo: ");
+      scanf("%d",&Classe);
+      printf("Digite a Area do Comodo: ");
+      scanf("%f",&Area);
+          if (Classe==1){
+              Potencia=vetor[0]*Area;
+          }
+          if (Classe==2){
+              Potencia=vetor[1]*Area;
+          }
+          if (Classe==3){
+              Potencia=vetor[2]*Area;
+          }
+          if (Classe==4){
+              Potencia=vetor[3]*Area;
+          }
+          if (Classe==5){
+              Potencia=vetor[4]*Area;
+          }
+      Lampadas=Potencia/60;
+      printf("Potencia do Comodo: %.f\n", Potencia);
+      printf("Lampadas Necessarias para o Comodo: %.f\n", Lampadas);
+      SomaPotencia=SomaPotencia+Potencia;
+      SomaLampadas=SomaLampadas+Lampadas;
+      Cont=Cont+1;
+  }
+  printf("Total de Potencia da Residencia: %.f\n", SomaPotencia);
+  printf("Total de Lampadas Necessarias para a Residencia: %.f\n", SomaLampadas);
 }
